df8db3a48bb676f8698e31c8c3018f57586d2db5
jecko2 <89352917+jecko2@users.noreply.github.com>
2021-09-01 06:16:31 +0300

    Update main.js

--- respon/main.js
+++ respon/main.js
@@ -17,34 +17,34 @@ function closeMenu(){
     navBar.classList.remove("active");
 }
 
-var slideImg = document.getElementById("slideImg");
+// var slideImg = document.getElementById("slideImg");
 
-var images = new Array(
-    // "respon/images/img1.jpg",
-    "/respon/img5.jpg",
-    "/respon/img6.jpg",
-    "/respon/img7r.jpg",
-    "/respon/xy1.jpg",
-    "/respon/xy2.jpg",
-    "/respon/xy3.jpg",
-    "/respon/xy4.jpg",
-    "/respon/xy5.jpg",
-    "/respon/xy6.jpg",
-    "/respon/xy7.jpg",
-    "/respon/xy8.jpg",
-    "/respon/xy9.jpg",
-    "/respon/xy10.jpg",
-)
-var len = images.length;
-var i = 0;
-function slider(){
-    if(i > len-1){
-        i=0;
-    }
-    slideImg.src = images[i];
-    i++;
-    setTimeout("slider()", 4500)
-}
+// var images = new Array(
+//     // "respon/images/img1.jpg",
+//     "/respon/img5.jpg",
+//     "/respon/img6.jpg",
+//     "/respon/img7r.jpg",
+//     "/respon/xy1.jpg",
+//     "/respon/xy2.jpg",
+//     "/respon/xy3.jpg",
+//     "/respon/xy4.jpg",
+//     "/respon/xy5.jpg",
+//     "/respon/xy6.jpg",
+//     "/respon/xy7.jpg",
+//     "/respon/xy8.jpg",
+//     "/respon/xy9.jpg",
+//     "/respon/xy10.jpg",
+// )
+// var len = images.length;
+// var i = 0;
+// function slider(){
+//     if(i > len-1){
+//         i=0;
+//     }
+//     slideImg.src = images[i];
+//     i++;
+//     setTimeout("slider()", 4500)
+// }
 
 
 
